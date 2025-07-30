244e5c4c79b86ee31d18353787abcbb5208e4e0f
gexu <gexu@singhand.com>
2025-07-30 17:48:02 +0800

    config(biz-service): 添加全局 CORS 配置
    - 实现了全局跨域资源共享（CORS）配置
    - 配置了允许所有来源、所有方法和所有头部的跨域请求
    - 添加了对 OPTIONS 请求的处理
    - 配置了未登录用户的认证逻辑
    - 注册了 SaInterceptor拦截器

---
+++ biz-service/src/main/java/com/gregory/satokendemo/bizservice/config/WebCorsConfig.java
@@ -0,0 +1,59 @@
+package com.gregory.satokendemo.bizservice.config;
+
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.context.model.SaResponse;
+import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaHttpMethod;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.json.JSONObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+  @Bean
+  public SaServletFilter getSaServletFilter() {
+
+    return new SaServletFilter()
+        .addInclude("/**")
+        .addExclude("/sso/**",
+            "/debug/**",
+            "/sa-res/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/doc.html",
+            "/favicon.ico")
+        .setBeforeAuth(obj -> {
+          SaHolder.getResponse()
+              .setHeader("Access-Control-Allow-Origin", "*")
+              .setHeader("Access-Control-Allow-Methods", "*")
+              .setHeader("Access-Control-Allow-Headers", "*")
+              .setHeader("Access-Control-Max-Age", "3600");
+          SaRouter.match(SaHttpMethod.OPTIONS).back();
+        })
+        .setAuth(obj -> {
+          if (!StpUtil.isLogin()) {
+            SaResponse response = SaHolder.getResponse();
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            SaRouter.back(new JSONObject(HttpStatus.UNAUTHORIZED.value()));
+          }
+        });
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+
+    registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+  }
+}
